dc0d81030f91e2f3f9a85bf3191e7552f2528093
Alexander Fakhrudinov <asbuka@gmail.com>
2022-09-03 11:49:20 +0300

    dz1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
-function App() {
+const _localConst = 123123
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header Back-color">
+        Some local const is {_localConst}
+        <h2 style={{color: 'red'}}>Header 2 with prop {props.nameOfProp}</h2>
+        <div>Date/Time is {props.dt}</div>
+        <p style={{color: props.paintColor || 'blue'}}>playig with css styles. Prop is {props.paintColor}</p>
+        <p>playig with props sended. Is 'paintColor' sended? {props.paintColor ? 'yes' : 'no'}</p>
       </header>
+
+      <div className="Back-color">I'm Message.js from App.Js!
+        <Message myNewProp={_localConst} />
+      </div>
     </div>
   );
 }
