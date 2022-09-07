a99aa3c991f3e345f60a2349a0e8d5eeb516c128
Alexander Fakhrudinov <asbuka@gmail.com>
2022-09-07 11:30:08 +0300

    dz2

--- src/index.js
+++ src/index.js
@@ -2,23 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Message from './Message';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const message = ReactDOM.createRoot(document.getElementById('message'));
 
-const myName = 'Alex';
-const dateTime = Date(Date.now());
-const color = 'green';
+const chatName = 'Message Form';
 
 root.render(
   <React.StrictMode>
-    <App nameOfProp={myName} dt={dateTime} paintColor={color} />
+    <App nameOfProp={chatName} />
   </React.StrictMode>
 );
 
-message.render(
-  <React.StrictMode>
-  <Message myNewProp={myName} />
-</React.StrictMode>
-);
